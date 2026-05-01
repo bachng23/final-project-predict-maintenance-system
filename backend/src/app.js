82f89281a1de372cbf09cd33c0024db04a9e85f3
marcusnk31 <vule63008@gmail.com>
2026-05-01 20:18:58 +0800

    feat(server): Initialize server (#2)
    * feat(server): Initialize server
    
    * fix(server): Add 404 error handling middleware
    
    Add middleware to handle 404 errors with a JSON response.
    
    ---------
    
    Co-authored-by: bachng23 <bachnguyen2326@gmail.com>

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const routes = require('./routes');
+
+const app = express();
+
+// Middlewares
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api', routes);
+// Xử lý Route Not Found (404)
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route không tồn tại!' });
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({
+    message: 'Something went wrong!',
+    error: process.env.NODE_ENV === 'development' ? err.message : {}
+  });
+});
+
+module.exports = app;
